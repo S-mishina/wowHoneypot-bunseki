f4f14b8a3c7ad4806af425b3ba21c392e5bd7ff2
S-mishina <183244r4@stu.teikyo-u.ac.jp>
2021-08-29 15:39:20 +0900

    ・bashの追加 ・requirements.txtの追加

---
+++ h_bash.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#git pull
+git -C ../../home/ssm-user/wowHoneypot-bunseki/ pull
+#h_ bash.sh chmod 707
+chmod 707 wowHoneypot-bunseki/h_ bash.sh
+#ここで本当はaws s3のinputをする必要がある.
+
+#ファイル名をここで決める
+to_day=$(date +"%Y%m%d")
+echo $to_day
+#pipの更新
+pip3 install -r ../../home/ssm-user/wowHoneypot-bunseki/requirements.txt
+#python jupiter出力
+jupyter nbconvert --execute ../../home/ssm-user/wowHoneypot-bunseki/test.ipynb --output output/$to_day  --to html
+#aws s3アップロード
+aws s3 cp ../../home/ssm-user/corona-bunseki/output s3://dev-honeypot-output-data/ --recursive
