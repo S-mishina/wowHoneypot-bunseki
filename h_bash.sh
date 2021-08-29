87b4199c2f8317d26cc73578308a2d1482df7cff
S-mishina <183244r4@stu.teikyo-u.ac.jp>
2021-08-29 22:27:26 +0900

    h_ bash.shの変更

--- h_bash.sh
+++ h_bash.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #git pull
 #git -C  wowHoneypot-bunseki/ pull
-git pull
+git pull -C ../../home/ssm-user/wowHoneypot-bunseki/ pull
 #ここで本当はaws s3のinputをする必要がある.
 #ファイル名をここで決める
 to_day=$(date +"%Y%m%d")
 echo $to_day
 #pipの更新
-pip3 install -rrequirements.txt
+pip3 install -r ../../home/ssm-user/wowHoneypot-bunseki/requirements.txt
 #python jupiter出力
-jupyter nbconvert --execute test.ipynb --output output/$to_day  --to html
+jupyter nbconvert --execute ../../home/ssm-user/wowHoneypot-bunseki/test.ipynb --output output/$to_day  --to html
 #aws s3アップロード
-aws s3 cp output s3://dev-honeypot-output-data/ --recursive
+aws s3 cp ../../home/ssm-user/wowHoneypot-bunseki/output s3://dev-honeypot-output-data/ --recursive
