8376dc049b30a0f5f9df48442cdf03fe8eba3ec5
S-mishina <183244r4@stu.teikyo-u.ac.jp>
2022-01-26 10:25:34 +0900

    コードの修正

--- h_bash.sh
+++ h_bash.sh
@@ -1,15 +1,15 @@
 !/bin/sh
 #git pull
 #git -C  wowHoneypot-bunseki/ pull
-git -C ../../home/ssm-user/wowHoneypot-bunseki/ pull
+git -C ../../../../home/ubuntu/wowHoneypot-bunseki/ pull
 #ここで本当はaws s3のinputをする必要がある.
-aws s3 cp s3://dev-honeypot-accesslog ../../home/ssm-user/wowHoneypot-bunseki/input/log --exclude "" --include ".txt" --recursive
+aws s3 cp s3://dev-honeypot-accesslog ../../../../home/ubuntu/wowHoneypot-bunseki/input/log --exclude"" --include ".txt" --recursive
 #ファイル名をここで決める
 to_day=$(date +"%Y%m%d")
 echo $to_day
 #pipの更新
-pip3 install -r ../../home/ssm-user/wowHoneypot-bunseki/requirements.txt
+pip3 install -r ../../../../home/ubuntu/wowHoneypot-bunseki/requirements.txt
 #python jupiter出力
-jupyter nbconvert --execute ../../home/ssm-user/wowHoneypot-bunseki/test.ipynb --output output/$to_day  --to html
+jupyter nbconvert --execute ../../../../home/ubuntu/wowHoneypot-bunseki/test.ipynb --output output/$to_day  --to html
 #aws s3アップロード
-aws s3 cp ../../home/ssm-user/wowHoneypot-bunseki/output s3://dev-honeypot-output-data/ --recursive
+aws s3 cp ../../../../home/ubuntu/wowHoneypot-bunseki/output s3://dev-honeypot-output-data/ --recursiv
